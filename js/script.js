fe8f9935bf116b30e85971019b5cf78acb2d4f52
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-09-27 14:53:14 +0100

    Finished Project

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ const formatValueDisplayFn = (moneyBacked, totalBackers) => {
 
     let decimal = moneyBacked / goal;
     let percentage = (decimal.toFixed(2) * 100).toString() + '%';
-    console.log(percentage);
     progressBar.style.removeProperty("width") 
     progressBar.style.setProperty("width", percentage) 
 
@@ -31,9 +30,72 @@ const formatValueDisplayFn = (moneyBacked, totalBackers) => {
     totalPeopleBacked.innerText = formattedBackers;
 }
 
+const formatStandsLeftFn = (numOfBamboo, numOfBlack, numOfMahogany) => {
+    numberOfBamboo.forEach((display) => {
+        if (numOfBamboo < 0) {
+            numOfBamboo = 0;
+        }
+        display.innerText = numOfBamboo;
+    })
+
+    numberOfBlack.forEach((display) => {
+        if (numOfBlack < 0) {
+            numOfBlack = 0;
+        }
+        display.innerText = numOfBlack;
+    })
+
+    numberOfMahogany.forEach((display) => {
+        if (numOfMahogany <= 0) {
+            numOfMahogany = 0;
+            outOfStockFn("mahogany");
+        }
+        display.innerText = numOfMahogany;
+    })
+}
+
+const outOfStockFn = (standType) => {
+    //num can't go below 0
+    //out of stock gets called a string "black", "bamboo"
+    //then switch case each case do appropriate styling;
+    
+    
+    switch (standType) {
+
+        case "bamboo":
+            selectBambooStand.classList.add("disable"); 
+            break;
+
+        case "black":
+            selectBlackStand.classList.add("disable"); 
+            break;
+
+        case "mahogany":
+            selectMahoganyButton.forEach((button) => {
+                button.disabled = "true";
+                button.innerText = "";
+                button.innerText = "Out of Stock"
+            })
+            
+            selectMahoganyContainer.forEach((container) => {
+                container.classList.add("opacity-50");
+                console.log(container)
+            })
+            
+            mahoganyHoverButton.forEach((hover) => {
+                hover.disabled = "true";
+                hover.classList.remove("group-hover:bg-teal-600")
+                hover.classList.remove("group-hover:text-teal-600")
+            })
+            break;
+    }
+
+}
+
 //math eventlisteners;
 window.addEventListener("load", () => {
     formatValueDisplayFn(moneyBacked, totalBackers);
+    formatStandsLeftFn(numOfBamboo, numOfBlack, numOfMahogany);
 })
 
 
@@ -46,10 +108,24 @@ const bookmarkSign = document.querySelector(".bookmark__sign");
 
 const totalMoneyBacked = document.getElementById("total-money-backed");
 const totalPeopleBacked = document.getElementById("total-backers");
+const numberOfBamboo = document.querySelectorAll(".number-of-bamboo"); 
+const numberOfBlack = document.querySelectorAll(".number-of-black"); 
+const numberOfMahogany = document.querySelectorAll(".number-of-mahogany"); 
 
 const inputPledge = document.querySelectorAll(".inputting-pledge");
 
 const progressBar = document.getElementById("progress-bar");
+
+const selectBambooStand = document.getElementById("select-bamboo-stand"); 
+const selectBlackStand = document.getElementById("select-black-stand"); 
+const selectMahoganyButton = document.querySelectorAll(".select-mahogany-button"); 
+
+const selectMahoganyContainer = document.querySelectorAll(".select-mahogany-container")
+
+const hamburgerButton = document.getElementById("hamburger-button");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const closeHamburgerButton = document.getElementById("close-hamburger-button");
+
 
 //main page functions
 const BookmarkFn = () => {
@@ -66,9 +142,22 @@ const BookmarkFn = () => {
     }
 }
 
+const HamburgerMenuFn = () => {
+    hamburgerMenu.classList.remove("hidden");
+    hamburgerButton.classList.add("hidden");
+    closeHamburgerButton.classList.remove("hidden"); 
+}
+
+const CloseHamburgerFn = () => {
+    hamburgerMenu.classList.add("hidden");
+    closeHamburgerButton.classList.add("hidden"); 
+    hamburgerButton.classList.remove("hidden");
+}
+
 //main page event listeners
 bookmarkButton.addEventListener("click", BookmarkFn);
-
+hamburgerButton.addEventListener("click", HamburgerMenuFn);
+closeHamburgerButton.addEventListener("click", CloseHamburgerFn);
 
 
 //modal button variables
@@ -103,8 +192,10 @@ const buttonMahoganyStand = document.getElementById("button-mahogany-stand");
 const mahoganyStandSelectionCircle = document.getElementById("mahogany-stand-selection");
 const hiddenMahoganyStandContent = document.getElementById("hidden-mahogany-content");
 const mahoganyInputAmount = document.getElementById("mahogany-input-amount");
+const mahoganyHoverButton = document.querySelectorAll(".hover-mahogany-button");
 
 const continueButton = document.querySelectorAll(".continue");
+
 
 //modal functions
 const NoRewardFn = () => {
@@ -138,7 +229,7 @@ const BlackStandFn = () => {
         blackStandSelectionCircle.classList.remove("bg-teal-600");
         hiddenBlackStandContent.classList.add("hidden");
     } else {
-        buttonBlackStand.classList.add("text-teal-600");
+        buttonBlackStand.classList.replace("text-black","text-teal-600");
         blackStandSelectionCircle.classList.add("bg-teal-600");
         hiddenBlackStandContent.classList.remove("hidden");
         blackInputAmount.focus();
@@ -201,6 +292,7 @@ modalOptions.forEach((option) => {
         })
         
         ResetModalOptionFn(modalContainer);
+        console.log(modalContainer)
         switch(option) {
             case buttonNoReward:
                 NoRewardFn();
@@ -230,6 +322,21 @@ continueButton.forEach((button) => {
         inputPledge.forEach((pledge) => {
             if (pledge.contains(button)) {
                 let addedMoney = pledge.childNodes[3].value;
+                let amountOfStands = pledge.childNodes[3].id;
+                
+                if (amountOfStands.includes("bamboo")) {
+                    numOfBamboo -= 1;
+                }
+
+                if (amountOfStands.includes("black")) {
+                    numOfBlack -= 1;
+                }
+
+                if (amountOfStands.includes("mahogany")) {
+                    numOfMahogany -= 1;
+                }
+
+                formatStandsLeftFn(numOfBamboo, numOfBlack, numOfMahogany);
                 updateValuesFn(addedMoney);
             }
         })
@@ -256,6 +363,9 @@ openModal.forEach((button) => {
             case "select-mahogany-stand":
                 MahoganyStandFn();
                 break;
+            default:
+                NoRewardFn();
+                break;
         }
 
     })
@@ -267,6 +377,7 @@ closeModal.addEventListener("click", () => {
 });
 
 closeModalConfirmation.addEventListener("click", () => {
+    ResetFn();
     modalConfirmation.close();
 })
 
